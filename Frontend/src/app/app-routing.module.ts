e0fc005cf6cdf527037bfcebe1d62f6a3f561e9c
Mauricio <mau.gar.gonz@iclod.com>
2019-02-09 08:33:19 -0600

    Para Mario
    ;)

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'card', loadChildren: './card/card.module#CardPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [
